565920d82e02d4c7d65bff9a143c3d05f0df1060
Oscar Olivas <oaolivas@hotmail.com>
2023-02-27 21:07:37 -0700

    Menu
    Menu con elementos editables

--- js/app.js
+++ js/app.js
@@ -12,12 +12,27 @@
 //     }
 // }
 
+
 function obtenerTexto(evento){
     console.log(evento)
     let nombreProducto = evento.target.innerText
     console.log(nombreProducto)
-    document.getElementsByTagName(nombreProducto).value = 'Cambio'
+    // console.log(document.getElementsByiD(nombreProducto))
 
-}
-document.querySelector('h6.comida').addEventListener('dblclick', obtenerTexto)
-document.querySelector('h6.ingrediente').addEventListener('dblclick', obtenerTexto)
+    // var celda = document.getElementById("td1");
+    // var header = document.createElement("h6");
+    // var textoHeader = document.createTextNode("Este es el encabezado");
+    // header.appendChild(textoHeader);
+    // celda.appendChild(header);
+
+    //let elemento = get
+  }
+
+
+//document.querySelector('h6.comida').addEventListener('dblclick', obtenerTexto)
+//document.querySelector('h6').addEventListener('dblclick', obtenerTexto)
+//document.getElementByTagName('h6').addEventListener('dblclick', obtenerTexto)
+//document.querySelector('h6').addEventListener('dblclick', obtenerTexto)
+
+var elemento = document.querySelector("h6")
+elemento.addEventListener("input", function(){})
