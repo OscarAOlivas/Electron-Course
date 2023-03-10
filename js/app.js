1646910f068cb72aac89ac31c3ff3c2479a6d0a7
Oscar Olivas <oaolivas@hotmail.com>
2023-03-10 12:23:57 -0700

    Actualizacion
    Se quito el boton de eliminar y se dejo el puro boton de agregar, se agrego la funcion del boton elminar del hover para que se elimine el elemento al presionarlo

--- js/app.js
+++ js/app.js
@@ -1,56 +1,33 @@
 
-
-// class TablaMenu {
-//     constructor(){
-//         this.tituloDesayunoD1 = document.getElementById('td1');
-
-//         this.agregarEventListeners();
-//     }
-
-//     agregarEventListeners(){
-//         this.tituloDesayunoD1.addEventListener('modificar')
-//     }
-// }
-
-
-function obtenerTexto(evento){
-    console.log(evento)
-    let nombreProducto = evento.target.innerText
-    console.log(nombreProducto)
-    // console.log(document.getElementsByiD(nombreProducto))
-
-    // var celda = document.getElementById("td1");
-    // var header = document.createElement("h6");
-    // var textoHeader = document.createTextNode("Este es el encabezado");
-    // header.appendChild(textoHeader);
-    // celda.appendChild(header);
-
-    //let elemento = get
-  }
-
-
-//document.querySelector('h6.comida').addEventListener('dblclick', obtenerTexto)
-//document.querySelector('h6').addEventListener('dblclick', obtenerTexto)
-//document.getElementByTagName('h6').addEventListener('dblclick', obtenerTexto)
-//document.querySelector('h6').addEventListener('dblclick', obtenerTexto)
-
 var elemento = document.querySelector("h6")
 var addButtons = document.querySelectorAll(".add-btn");
-var deleteButtons = document.querySelectorAll(".del-btn");
 var celdaActual
+var closeButtonActual
 var celdas = document.querySelectorAll("td.dat")
 
-elemento.addEventListener("input", function(){})
+// elemento.addEventListener("input", function(){
+//     console.log('entro')
+//     var valor = $(this).val();
+//     if (valor === '') {
+//       $('.ingrediente').text('');
+//     } else {
+//       $('.ingrediente').text(valor);
+//     }
+// })
 
 celdas.forEach(celda =>{
     celda.addEventListener("mouseover",function(){
-        //console.log(celda)
         var addButton = celda.querySelector(".add-btn")
-        var deleteButton = celda.querySelector(".del-btn")
-        console.log(deleteButton)
+        getCloseButtons = celda.querySelectorAll("span.close-button")
         addButton.style.display = "flex"
-        deleteButton.style.display = "flex"
         celdaActual = celda
+
+        getCloseButtons.forEach(closeButton =>{
+            closeButton.addEventListener("click",function(){
+                eliminarIngrediente = this.parentNode;
+                eliminarIngrediente.remove()           
+            })
+        })  
      })
 })
 
@@ -58,10 +35,7 @@ celdas.forEach(celda =>{
 celdas.forEach(celda => {
     celda.addEventListener("mouseout",function(){
         var addButton = celda.querySelector(".add-btn")
-        var deleteButton = celda.querySelector(".del-btn")
         addButton.style.display = "none"
-        deleteButton.style.display = "none"
-    
     })
 })
 
@@ -71,7 +45,6 @@ addButtons.forEach(boton =>{
         var nuevoIngrediente = document.createElement("h6")
         var nuevoDiv = document.createElement("div")
         var nuevoSpan = document.createElement("span")
-        var deleteButton = celdaActual.querySelector(".del-btn")
         nuevoIngrediente.classList.add("ingrediente")
         nuevoIngrediente.setAttribute("contenteditable", "true")
         var contenido = document.createTextNode("ingrediente")
@@ -91,8 +64,8 @@ addButtons.forEach(boton =>{
         celdaActual.appendChild(nuevoDiv)
         divActual = celdaActual.querySelector(".divbotones")
         divActual.appendChild(boton)
-        divActual.appendChild(deleteButton)
-
-    
      })
 })
+
+
+
